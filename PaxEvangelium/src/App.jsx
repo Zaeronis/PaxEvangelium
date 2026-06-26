1b1089a9e105b570e5dc60badde67914569e62ba
Zaeronis <hunter.dalton81396@gmail.com>
2026-06-25 22:19:50 -0500

    Migrate project to React with Vite and Oxlint configuration
    Signed-off-by: Zaeronis <hunter.dalton81396@gmail.com>

---
+++ PaxEvangelium/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <img
+            src="/pax.png"
+            alt="Pax Evangelium Logo"
+            className="logo"
+        />
+        <h1>Pax Evangelium</h1>
+        <p>An all-in-one platform for spiritual utility and community.</p>
+        <p><em>Coming Soon.</em></p>
+      </div>
+    </>
+  )
+}
+
+export default App
