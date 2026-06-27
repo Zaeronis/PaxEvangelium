411d917bfd26127df56e569b66fea28f248bf16f
Zaeronis <hunter.dalton81396@gmail.com>
2026-06-26 19:27:57 -0500

    Add Ephesians 6 component and update styles for landing page layout and design
    Signed-off-by: Zaeronis <hunter.dalton81396@gmail.com>

--- PaxEvangelium/src/App.jsx
+++ PaxEvangelium/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import EphesiansSix from "./assets/pages/landing/com/EphesiansSix.jsx";
 
 function App() {
 
@@ -10,7 +11,12 @@ function App() {
             alt="Pax Evangelium Logo"
             className="logo"
         />
-        <h1>Pax Evangelium</h1>
+        <h1 className="title">Pax Evangelium</h1>
+        <br/>
+        <br/>
+        <EphesiansSix />
+        <br/>
+        <br/>
         <p>An all-in-one platform for spiritual utility and community.</p>
         <p><em>Coming Soon.</em></p>
       </div>
---
+++ PaxEvangelium/src/assets/pages/landing/com/EphesiansSix.jsx
@@ -0,0 +1,14 @@
+import '../style/EphesiansSix.css'
+
+export default function EphesiansSix() {
+    return (
+        <>
+            <p className="verse">
+                <span class="verseName">Ephesians 6:13–17</span>
+                <br />
+                <br />
+                ¹³Therefore put on the full armor of God, so that when the day of evil comes, you may be able to stand your ground, and after you have done everything, to stand. ¹⁴Stand firm then, with the belt of truth buckled around your waist, with the breastplate of righteousness in place, ¹⁵and with your feet fitted with the readiness that comes from <span className="pax">the gospel of peace.</span> ¹⁶In addition to all this, take up the shield of faith, with which you can extinguish all the flaming arrows of the evil one. ¹⁷Take the helmet of salvation and the sword of the Spirit, which is the word of God.
+            </p>
+        </>
+    )
+}
